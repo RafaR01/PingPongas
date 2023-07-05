b7ea5c1dec550ba0c03413e58e582c71dc81d660
t0t0cas <antonio.ramalho.fialho@gmail.com>
2023-07-05 23:41:51 +0100

    Criação das ligações para adicionar partida e seleção de adversário

--- app/src/main/java/pt/ipt/dama/pingpongas/AdicionarPartida.kt
+++ app/src/main/java/pt/ipt/dama/pingpongas/AdicionarPartida.kt
@@ -1,11 +1,43 @@
 package pt.ipt.dama.pingpongas
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.Spinner
+import android.widget.Toast
 
 class AdicionarPartida : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_adicionar_partida)
+
+        /**
+         * Inicialização do botão e os elementos da selected list
+         */
+        var btnNovaPartida : Button = findViewById(R.id.novapartida);
+        val selecUtilizadores = findViewById<Spinner>(R.id.selectadvr);
+        val listaUtilizadores = resources.getStringArray(R.array.adversarios);
+
+        btnNovaPartida.setOnClickListener {
+            criaPartida();
+        }
+
+        if(selecUtilizadores != null){
+            val utilizadores = ArrayAdapter(this, android.R.layout.simple_spinner_item, listaUtilizadores);
+            utilizadores.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            selecUtilizadores.adapter = utilizadores;
+        }
+        fun mostraValor(view : View){
+            Toast.makeText(this, "Seleciona Adversário " + selecUtilizadores.selectedItem.toString(),Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private fun criaPartida(){
+        val intent = Intent(this, AdicionarPartida2::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/pt/ipt/dama/pingpongas/AdicionarPartida2.kt
+++ app/src/main/java/pt/ipt/dama/pingpongas/AdicionarPartida2.kt
@@ -1,11 +1,24 @@
 package pt.ipt.dama.pingpongas
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class AdicionarPartida2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_adicionar_partida2)
+
+        var btnAdicionarPartida : Button = findViewById(R.id.partida);
+
+        btnAdicionarPartida.setOnClickListener{
+            adicionaPartida();
+        }
+    }
+
+    private fun adicionaPartida(){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
