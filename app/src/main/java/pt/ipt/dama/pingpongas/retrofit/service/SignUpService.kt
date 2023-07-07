39a9137044bdf2c9b20d0c44c9e8876c388883cf
RafaR01 <80701627+RafaR01@users.noreply.github.com>
2023-07-07 02:17:08 +0100

    Squashed commit of the following:
    commit 27d323dde0bc9dc87e3ce1fa8b1b851dcb85d08f
    Author: RafaR01 <80701627+RafaR01@users.noreply.github.com>
    Date:   Fri Jul 7 02:15:35 2023 +0100
    
        Squashed commit of the following:
    
        commit c34c82d4d3a32b18f0de097bda2e2e6d2f16e9e4
        Author: RafaR01 <80701627+RafaR01@users.noreply.github.com>
        Date:   Fri Jul 7 02:09:00 2023 +0100
    
            Mostrar as "Stats" na activity do Perfil dos utilizadores com recurso à API
    
        commit 6ee1fbef6d205752d6d85fd0ddc04609fc91c767
        Merge: 69217d4 95708b8
        Author: RafaR01 <80701627+RafaR01@users.noreply.github.com>
        Date:   Thu Jul 6 17:22:14 2023 +0100
    
            Merge branch 'main' into Rafael
    
        commit 69217d44babc3fd02f4d4ebaf9ffad42b990a43e
        Author: RafaR01 <80701627+RafaR01@users.noreply.github.com>
        Date:   Thu Jul 6 14:17:21 2023 +0100
    
            Front-end e Backend do Registo e Login com Recurso à API
    
    commit d7455a61a9a749b6c31c8781e96ea7d77a8a94ad
    Merge: 14272ed 6e77c6e
    Author: António Fialho <80701611+t0t0cas@users.noreply.github.com>
    Date:   Fri Jul 7 01:57:12 2023 +0100
    
        Merge pull request #8 from RafaR01/Antonio
    
        Inicio da ligação à API para enviar a fotografia do user
    
    commit 6e77c6e382e4c57aac3b9a2766a1fb698f4135ce
    Author: António Fialho <antonio.ramalho.fialho@gmail.com>
    Date:   Fri Jul 7 01:56:29 2023 +0100
    
        Inicio da ligação à API para enviar a fotografia do user
    
    commit 14272ed92dc12e763f13172e331fcf17ec0abbc4
    Merge: 95708b8 02e76e6
    Author: António Fialho <80701611+t0t0cas@users.noreply.github.com>
    Date:   Thu Jul 6 18:22:44 2023 +0100
    
        Merge pull request #7 from RafaR01/Antonio
    
        Fotografias serem uploaded por imagens da galeria
    
    commit 02e76e65b8448a579420f48a47b3da046411414a
    Author: António Fialho <antonio.ramalho.fialho@gmail.com>
    Date:   Thu Jul 6 18:20:02 2023 +0100
    
        Minor error
    
    commit b61fafa5431a54f5acae1a30c57043cb1743669f
    Author: António Fialho <antonio.ramalho.fialho@gmail.com>
    Date:   Thu Jul 6 18:19:38 2023 +0100
    
        Fotografias podem ser uploaded por imagens que constam na galeria

--- app/src/main/java/pt/ipt/dama/pingpongas/retrofit/service/SignUpService.kt
+++ app/src/main/java/pt/ipt/dama/pingpongas/retrofit/service/SignUpService.kt
@@ -3,11 +3,15 @@ package pt.ipt.dama.pingpongas.retrofit.service
 import pt.ipt.dama.pingpongas.model.LoginData
 import pt.ipt.dama.pingpongas.model.SignUpData
 import pt.ipt.dama.pingpongas.model.SignUpResult
+import pt.ipt.dama.pingpongas.model.imageData
+import pt.ipt.dama.pingpongas.model.imageResponse
 import pt.ipt.dama.pingpongas.model.StatsData
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Multipart
 import retrofit2.http.POST
+import retrofit2.http.Part
 import retrofit2.http.Path
 
 interface SignUpService {
@@ -39,4 +43,10 @@ interface SignUpService {
      */
     @POST("users/nao_interessa_a_ninguem")
     fun addUser(@Body data:SignUpData): Call<SignUpResult>
+
+    @Multipart
+    @POST("upload/9/nao_interessa_a_ninguem")
+    fun uploadImage(@Part("imagem") imagePart: imageData): Call<imageResponse>
+
+
 }
